ba01c5d5f030e8bfef0109969118377b802e0678
José Ignacio Escribano <jiep12345@hotmail.com>
2015-10-26 19:11:44 +0100

    Empezando memoria

--- caso_iv.R
+++ caso_iv.R
@@ -1,13 +1,45 @@
-# Cargamos el archivo "birdextinct.txt"
+# Cargamos el archivo "BHD.txt"
 
-birdextinct <- read.csv(file.choose(),header = TRUE, sep = "\t")
+bhd <- read.csv(file.choose(),header = FALSE, sep = "\t")
 
-# Definimos las variables
-time <- birdextinct$time
-nesting <- birdextinct$nesting
-size <- birdextinct$size
-status <- birdextinct$status
+names(bhd) <- c("CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", 
+  "DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT", "MEDV")
 
-# Calculamos el modelo lineal
-linearmodel1 <- lm(log(time) ~ nesting + size + status)
-summary(linearmodel1)$r.squared
+# Cargamos la librería LearnBayes
+library(LearnBayes)
+
+# Observamos los primeros datos del archivo
+head(bhd)
+
+# Vinculamos los nombres de las variables a los datos
+attach(bhd)
+
+# Resumen de cada una de las variables
+summary(bhd)
+
+# Hacemos algunos gráficos de las variables
+# par(mfrow=c(2,7))
+for(i in length(bhd)){
+  hist(bhd[,i], xlab = names(bhd[,i]), ylab = "Frecuencia", main = "Histograma de ", names(bhd[,i]))
+}
+
+
+model <- lm(MEDV ~. , data = bhd, x = TRUE, y = TRUE)
+summary(model)
+
+theta.sample <- blinreg(model$y, model$x, 5000)
+
+S <- sum(model$residuals^2)
+shape <- model$df.residual/2
+rate <- S/2
+sigma2 <- rigamma(1, shape, rate)
+
+MSE <- sum(model$residuals^2)/model$df.residual
+vbeta <- vcov(model)/MSE
+beta <- rmnorm(1, mean = model$coef, varcov= vbeta*sigma2)
+
+apply(theta.sample$beta, 2, quantile, c(0.05, 0.5, 0.95))
+
+quantile(theta.sample$sigma, c(0.05, 0.5, 0.95))
+
+prob.out <- bayesresiduals(model, theta.sample, 2)
